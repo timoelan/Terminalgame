3350ddbf0961d82aa7bbb9d2c8866b7caec966f2
TAAEITI1 <timo.limmat@gmail.com>
2024-10-21 08:45:20 +0200

    can play the game to a point

--- src/main/java/org/example/StoryTeller.java
+++ src/main/java/org/example/StoryTeller.java
@@ -1,59 +1,88 @@
 package org.example;
-
-import java.util.Map;
-import java.util.Scanner;
-
+import java.util.*;
 public class StoryTeller {
-
-    // currentRoomID speichern damit ich weiss welcher Raum ich momentan bin.
     String currentRoomID = null;
-
-    // das ist dre Map string wie aus dem yaml file. Hier sieht man Game.Room> rooms. Das sind alle Rooms drin.
     StoryReader.Game game;
-
-    // Constructor  hier übergebe ich die Map Rooms dem Constructor und die ID des jetztigem Room ID
-
+    Set<String> states = new HashSet<>();
+    Set<String> bag = new HashSet<>();
+    boolean resentChangedRoom;
 
     public StoryTeller(StoryReader.Game game) {
         this.game = game;
         this.currentRoomID = game.startRoom();
     }
 
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_GREEN = "\u001B[32m";
     public void startGame(){
-        System.out.println(game.description());
+
+        System.out.println(ANSI_GREEN + game.description() + ANSI_RESET);
         Scanner scanner = new Scanner(System.in);
 
-        while (true){
-            StoryReader.Game.Room currentRoom = game.rooms().get(currentRoomID);
-            System.out.println(currentRoom.description());
 
+        while (true) {
+
+            StoryReader.Game.Room currentRoom = game.rooms().get(currentRoomID);
+            if (resentChangedRoom) {
+                System.out.println(currentRoom.description());
+                resentChangedRoom = false;
+            }
             System.out.print(currentRoom.name() + "  >> ");
             String input = scanner.nextLine();
 
-            ProccesInput(input, currentRoom);
+            String[] parsedInput = ProccesInput(input, currentRoom);
 
+            String verb = parsedInput[0];
+            Map<String, List<StoryReader.Game.Room.Action>> currentVerb = currentRoom.verbs().get(verb);
+
+            if (currentVerb == null) {
+                System.out.println("That Verb doesn't exist");
+                continue;
+            }
+
+            boolean runNextAction = true;
+            String object = parsedInput[1];
+            List<StoryReader.Game.Room.Action> currentActions = currentVerb.get(object);
+            if (currentActions == null) {
+                System.out.println(currentVerb.keySet());
+                continue;
+            }
+
+            for (StoryReader.Game.Room.Action activeAction : currentActions) {
+                if (!runNextAction) break;
+                String ifState = activeAction.ifState();
+                if (ifState != null) {
+                    if (!bag.contains(ifState)) {
+                        continue;
+                    }
+                }
+
+                String message = activeAction.message();
+                if (message != null) {
+                    System.out.println(message);
+                }
+
+                if (activeAction.room() != null){
+                    currentRoomID = activeAction.room();
+                    resentChangedRoom = true;
+                }
+
+                String addState = activeAction.addState();
+                if (addState != null) {
+                    if (!bag.contains(addState)) {
+                        bag.add(addState);
+                    }
+                }
+                runNextAction = false;
+            }
         }
-
-
     }
-    private void ProccesInput(String input, StoryReader.Game.Room room){
+    private String[] ProccesInput(String input, StoryReader.Game.Room room) {
         String[] parts = input.split(" ");
-
-        if (parts.length < 2){
-            System.out.println("Ungültiger Befehl. Bitte <verb object> verwenden.");
-            return;
-        }
         String verb = parts[0];
-        String object = parts[1];
-
-        if (!room.verbs().containsKey(verb)) {
-            System.out.println("Du kannst das nicht tun.");
-            return;
-        }
-
+        String object = parts.length > 1 ? parts[1] : "";
+        return new String[]{verb, object};
     }
-
-
 
 
 }
